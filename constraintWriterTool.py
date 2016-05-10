b442943e1c73293f0e5e2b1d2280304ea96898d7
enkiv2 <john.ohno@gmail.com>
2016-05-10 13:48:55 -0400

    support compiling multiple corpus files to a single bigram list

--- constraintWriterTool.py
+++ constraintWriterTool.py
@@ -5,7 +5,7 @@ import os, sys
 from sys import argv, exit
 
 def printUsage():
-	print("Usage: constraintWriterTool action [options]\nActions:\n\tsuggest\t\tbigramfile word\n\tinWhitelist\tbigramfile word\n\tinBlacklist\tbigramfile word\n\tcompile\t\tcorpus bigramfile\n")
+	print("Usage: constraintWriterTool action [options]\nActions:\n\tsuggest\t\tbigramfile word\n\tinWhitelist\tbigramfile word\n\tinBlacklist\tbigramfile word\n\tcompile\t\tcorpus bigramfile\n\tcompileMulti\tbigramfile corpus [corpus_2 ... corpus_n]\n")
 	exit(1)
 
 if len(argv)<4:
@@ -23,4 +23,10 @@ elif argv[1]=="compile":
 	with open(argv[2], 'r') as f:
 		saveBigrams(corpus2bigrams(f.read()), argv[3])
 
+elif argv[1]=="compileMulti":
+	corpora=[]
+	for fname in argv[3:]:
+		with open(fname, 'r') as f:
+			corpora.append(f.read())
+	saveBigrams(corpus2bigrams("\n".join(corpora)), argv[2])
 	
