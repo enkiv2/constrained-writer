0827911184bf43a6dd50712444d3f9385a64eb31
enkiv2 <john.ohno@gmail.com>
2016-05-20 13:25:58 -0400

    support combining bigrams

--- constraintWriterTool.py
+++ constraintWriterTool.py
@@ -5,7 +5,7 @@ import os, sys
 from sys import argv, exit
 
 def printUsage():
-	print("Usage: constraintWriterTool action [options]\nActions:\n\tsuggest\t\tbigramfile word\n\tsuggestPfx\t\tbigramfile word prefix\n\tinWhitelist\tbigramfile word\n\tinBlacklist\tbigramfile word\n\tcompile\t\tcorpus bigramfile\n\tcompileMulti\tbigramfile corpus [corpus_2 ... corpus_n]\n")
+	print("Usage: constraintWriterTool action [options]\nActions:\n\tsuggest\t\tbigramfile word\n\tsuggestPfx\tbigramfile word prefix\n\tinWhitelist\tbigramfile word\n\tinBlacklist\tbigramfile word\n\tcompile\t\tcorpus bigramfile\n\tcompileMulti\tbigramfile corpus [corpus_2 ... corpus_n]\n\tcombine\t\tbigramfile_out [bigramfile_in ... ]\n")
 	exit(1)
 
 if len(argv)<4:
@@ -31,4 +31,18 @@ elif argv[1]=="compileMulti":
 		with open(fname, 'r') as f:
 			corpora.append(f.read())
 	saveBigrams(corpus2bigrams("\n".join(corpora)), argv[2])
+elif argv[1]=="combine":
+	bigrams={}
+	for fname in argv[3:]:
+		world=loadBigrams(fname)
+		for w1 in world.keys():
+			if not (w1 in bigrams):
+				bigrams[w1]={}
+			for w2 in world[w1].keys():
+				if not w2 in bigrams[w1]:
+					bigrams[w1][w2]=0
+				bigrams[w1][w2]+=world[w1][w2]
+	saveBigrams(bigrams, argv[2])
+
+				
 	
